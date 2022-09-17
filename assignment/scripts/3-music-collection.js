b189ec724a79f3c1409d185411547bc6e72e8f52
Jordan Wolter <jordan.wolter@icloud.com>
2022-09-17 12:51:50 -0500

    i think i finished the addalbum function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,15 +3,22 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection( title, artist, yearPublished ){
+    const album = {
+        title : title,
+        artist : artist,
+        yearPublished : yearPublished
+    }
+    
+    collection.push( album );
 
+    return album;
 }
 
-const albumOne = {
-    title : 'Three Cheers For Sweet Revenge',
-    artist : 'My Chemical Romance',
-    yearPublished : '2004'
-}
-
-collection.push(albumOne);
+addToCollection( 'Three Cheers For Sweet Revenge', 'My Chemical Romance', '2004' );
+addToCollection( 'Sometimes Things Just Disappear', 'Polar Bear Club', '2008' );
+addToCollection( 'The Black Parade', 'My Chemical Romance', '2006' );
+addToCollection( 'Recess', 'Skrillex', '2014' );
+addToCollection( 'Somewhere Else', 'Zeds Dead', '2014' );
+addToCollection( 'Crybaby', 'Lil Peep', '2016' );
 
 console.log(collection);
