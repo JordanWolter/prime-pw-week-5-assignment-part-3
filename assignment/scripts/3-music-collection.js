639cd12ade858ad622090b92c4fe44dfa9c5642d
Jordan Wolter <jordan.wolter@icloud.com>
2022-09-17 12:20:04 -0500

    added an album

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,17 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection( title, artist, yearPublished ){
+
+}
+
+const albumOne = {
+    title : 'Three Cheers For Sweet Revenge',
+    artist : 'My Chemical Romance',
+    yearPublished : '2004'
+}
+
+collection.push(albumOne);
+
+console.log(collection);
